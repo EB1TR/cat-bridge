a0a0716153edf091dff91da0aa3bf7a224c93cff
EB1TR <fabian@eb1tr.com>
2024-12-07 18:17:39 +0100

    Refactor
    Legibilidad de código
    Paquetería
    Funciones

--- sniffer-dosificador-simple.py
+++ sniffer-dosificador-simple.py
@@ -31,6 +31,7 @@ try:
     MQ_PORT = config["mq_port"]
     MQ_TOPIC = config["mq_topic"]
     DEBUG = config["debug"]
+    VERSION = "V20241206"
 except Exception as e:
     print("No es posible abrir el fichero de configuración o el formato es incorrecto")
     print(e)
@@ -38,43 +39,49 @@ except Exception as e:
     sys.exit(0)
 # ----------------------------------------------------------------------------------------------------------------------
 
-print(f'Iniciando CAT-Bridge V20241014 by EB1TR')
+# Inicialización serie -------------------------------------------------------------------------------------------------
+#
+def do_serial():
+    try:
+        sera = serial.Serial(port=RX_PORT)
+        print(f'Configurando puerto TX/RX del TRX: {RX_PORT}')
+        sera.baudrate = RX_BAUD
+        print(f'Configurando velocidad TX/RX del TRX: {RX_BAUD}')
+        sera.bytesize = serial.EIGHTBITS
+        sera.parity = serial.PARITY_NONE
+        sera.stopbits = serial.STOPBITS_TWO
+        sera.setRTS = False
+        sera.setDTR = False
+        sera.rts = False
+        sera.dtr = False
+        sera.rtscts = False
+        sera.dsrdtr = False
+        sera.timeout = 1
+        serb = serial.Serial(port=TX_PORT)
+        print(f'Configurando puerto TX del PA: {TX_PORT}')
+        serb.baudrate = TX_BAUD
+        print(f'Configurando velocidad TX del PA: {TX_BAUD}')
+        serb.bytesize = serial.EIGHTBITS
+        serb.parity = serial.PARITY_NONE
+        serb.stopbits = serial.STOPBITS_TWO
+        serb.setRTS = False
+        serb.setDTR = False
+        serb.rts = False
+        serb.dtr = False
+        serb.rtscts = False
+        serb.dsrdtr = False
+        return sera, serb
+    except Exception as e:
+        print("No es posible abrir uno o mas puertos")
+        print(e)
+        input("Presione ENTER para salir...")
+        sys.exit(0)
+# ----------------------------------------------------------------------------------------------------------------------
 
-try:
-    sera = serial.Serial(port=RX_PORT)
-    print(f'Configurando puerto TX/RX del TRX: {RX_PORT}')
-    sera.baudrate = RX_BAUD
-    print(f'Configurando velocidad TX/RX del TRX: {RX_BAUD}')
-    sera.bytesize = serial.EIGHTBITS
-    sera.parity = serial.PARITY_NONE
-    sera.stopbits = serial.STOPBITS_TWO
-    sera.setRTS = False
-    sera.setDTR = False
-    sera.rts = False
-    sera.dtr = False
-    sera.rtscts = False
-    sera.dsrdtr = False
-    sera.timeout = 1
-    serb = serial.Serial(port=TX_PORT)
-    print(f'Configurando puerto TX del PA: {TX_PORT}')
-    serb.baudrate = TX_BAUD
-    print(f'Configurando velocidad TX del PA: {TX_BAUD}')
-    serb.bytesize = serial.EIGHTBITS
-    serb.parity = serial.PARITY_NONE
-    serb.stopbits = serial.STOPBITS_TWO
-    serb.setRTS = False
-    serb.setDTR = False
-    serb.rts = False
-    serb.dtr = False
-    serb.rtscts = False
-    serb.dsrdtr = False
-except Exception as e:
-    print("No es posible abrir uno o mas puertos")
-    print(e)
-    input("Presione ENTER para salir...")
-    sys.exit(0)
 
-if MQ_DATA:
+# Conexión MQTT --------------------------------------------------------------------------------------------------------
+#
+def do_mqtt():
     try:
         print(f'Broker MQTT: Activado')
         client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
@@ -93,88 +100,107 @@ if MQ_DATA:
                 input("Presione ENTER para salir...")
                 sys.exit(0)
         print(f'Broker MQTT (estado): Conectado')
+        return client
     except Exception as e:
         print("No es posible abrir la conexión MQTT")
         print(e)
         input("Presione ENTER para salir...")
         sys.exit(0)
-else:
-    print(f'Broker MQTT - Host: Desactivado')
-
-msg = ""
-data_a = ""
-amp_data = ""
-fa_time = 0
-fa_last_send_time = time.time()
-fa_send_interval_time = TX_TIME
-fa_expire_time = EX_DATA
-
-print(f'Intervalo entre envíos: {int(fa_send_interval_time*1000)} milisegundos')
-print(f'Tiempo de expiración de frecuencia: {int(fa_expire_time*1000)} milisegundos')
-time.sleep(3)
+# ----------------------------------------------------------------------------------------------------------------------
 
 
+# Abrir e importar configuración ---------------------------------------------------------------------------------------
+#
 def to_ts(ts_epoch):
     try:
         ts = datetime.datetime.fromtimestamp(ts_epoch).strftime('%H:%M:%S')
         return ts
     except:
         return 'no time'
+# ----------------------------------------------------------------------------------------------------------------------
 
 
-while True:
+# Loop principal -------------------------------------------------------------------------------------------------------
+#
+def do_sniffer():
     try:
-        # Calculamos si es necesario volver a enviar un dato o es necesario actualizarlo previamente
-        fa_send = fa_last_send_time + fa_send_interval_time <= time.time()
-        fa_expired = fa_time + fa_expire_time <= time.time()
-        # Sniffer of the PC<->Rig
-        time.sleep(0.01)
-        # Recepción de nuevos paquetes
-        if sera.inWaiting() > 0:
-            data_a = sera.read_until(b';')
-            data_a = data_a.decode('utf-8')
-            # Comprobación de datos válidos
-            is_if = bool(re.search(r'IF\d{11}.*;', data_a))
-            is_fa = bool(re.search(r'FA\d{11}.*;', data_a))
-            if is_if or is_fa:
-                qrg_data = re.findall(r'(\d{11})|\d{11}.*', data_a)[0]
-                amp_data = "FA%s;" % qrg_data
-                fa_time = time.time()
-                msg = "Sniffed"
-                if DEBUG > 0:
-                    print(f'RAW DATA SNIFFED: {data_a}')
-                print("RXA | %s | %s | %s" % (msg, to_ts(fa_last_send_time), amp_data))
-                if MQ_DATA:
-                    client.publish(f'{MQ_TOPIC}/raw_if', amp_data)
+        # Se inicializan ambos puertos serie
+        serials = do_serial()
+        sera = serials[0]
+        serb = serials[1]
 
-        # Enviamos datos que si es necesario enviar
-        if fa_send:
-            # Si los datos existentes han caducado se solicitan nuevos
-            if fa_expired:
-                sera.write(b'FA;')
-                print("-" * 42)
-                print(f'TXA | Poll    | {to_ts(int(time.time()))} | Polling...')
-                print("-" * 42)
+        amp_data = ""
+        fa_time = 0
+        fa_last_send_time = time.time()
+        fa_send_interval_time = TX_TIME
+        fa_expire_time = EX_DATA
+        requested = False
+        print(f'Intervalo entre envíos: {int(fa_send_interval_time * 1000)} milisegundos')
+        print(f'Tiempo de expiración de frecuencia: {int(fa_expire_time * 1000)} milisegundos')
+        time.sleep(1)
+
+        if MQ_DATA:
+            mqtt_client = do_mqtt()
+        else:
+            print(f'Broker MQTT - Host: Desactivado')
+
+        while True:
+            # Sniffer of the PC<->Rig
+            # Recepción de nuevos paquetes
+            if sera.inWaiting() > 0:
                 data_a = sera.read_until(b';')
                 data_a = data_a.decode('utf-8')
+                # Comprobación de datos válidos
+                is_if = bool(re.search(r'IF\d{11}.*;', data_a))
                 is_fa = bool(re.search(r'FA\d{11}.*;', data_a))
-                if is_fa:
-                    qrg_data = re.findall(r'(\d{11})', data_a)[0]
+                if is_if or is_fa:
+                    qrg_data = re.findall(r'(\d{11})|\d{11}.*', data_a)[0]
                     amp_data = "FA%s;" % qrg_data
                     fa_time = time.time()
-                    msg = "Polled  "
                     if DEBUG > 0:
                         print(f'RAW DATA SNIFFED: {data_a}')
-                    print("RXA | Polled   | %s | %s" % (to_ts(fa_last_send_time), amp_data))
+                    print("RXA | %s | %s" % (to_ts(fa_last_send_time), amp_data))
+                    if MQ_DATA:
+                        mqtt_client.publish(f'{MQ_TOPIC}/raw_if', amp_data)
+                    requested = False
 
-            # Enviamos los datos al puerto serie
-            if bool(re.search(r'FA\d{11}.*;', amp_data)):
-                serb.write(amp_data.encode('utf-8'))
-                if MQ_DATA:
-                    client.publish(f'{MQ_TOPIC}/raw_if', amp_data)
-                print("TXB | %s | %s | %s" % (msg, to_ts(fa_last_send_time), amp_data))
-                fa_last_send_time = time.time()
-            # Publicamos también en MQTT
+            # Calculamos si es necesario volver a enviar un dato o es necesario actualizarlo previamente
+            fa_send = fa_last_send_time + fa_send_interval_time <= time.time()
+            fa_expired = fa_time + fa_expire_time <= time.time()
 
+            # Enviamos datos que si es necesario enviar
+            if fa_send:
+                # Si los datos existentes han caducado se solicitan nuevos
+                if not fa_expired and bool(re.search(r'FA\d{11}.*;', amp_data)):
+                    serb.write(amp_data.encode('utf-8'))
+                    if MQ_DATA:
+                        mqtt_client.publish(f'{MQ_TOPIC}/raw_if', amp_data)
+                    print("TXB | %s | %s" % (to_ts(fa_last_send_time), amp_data))
+                    fa_last_send_time = time.time()
+                elif not requested:
+                    sera.write(b'FA;')
+                    requested = True
+                    print(f'TXA | {to_ts(int(time.time()))} | Polling...')
+
+    except KeyboardInterrupt:
+        print(f'\n** Salida por el usuario.')
+        mqtt_client.disconnect()
+        sys.exit(0)
+    except EOFError:
+        print(f'\n** Cerrando el programa debido a EOFError: {EOFError}')
+        mqtt_client.disconnect()
+        sys.exit(1)
+    except OSError:
+        print(f'\n** Cerrando el programa debido a OSError: {OSError}')
+        mqtt_client.disconnect()
+        sys.exit(1)
     except Exception as e:
+        print(f'\n** Cerrando el programa debido a una excepción:')
         print(e)
+        mqtt_client.disconnect()
+        sys.exit(0)
+# ----------------------------------------------------------------------------------------------------------------------
+
+if __name__ == '__main__':
+    print(f'Iniciando CAT-Bridge {VERSION} by EB1TR')
+    do_sniffer()
